82cbb67dbd9101b7fdd87ae796e039eff9a83364
dhanushtheijas08 <21cs033@kpriet.ac.in>
2024-05-09 14:47:25 +0530

    feat: Update map zoom level to 5 for better visibility

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -19,7 +19,7 @@ const createClusterCustomIcon = function (cluster) {
 
 const Map = ({ markers }) => {
   return (
-    <MapContainer center={[48.8566, 2.3522]} zoom={13}>
+    <MapContainer center={[48.8566, 2.3522]} zoom={5}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
