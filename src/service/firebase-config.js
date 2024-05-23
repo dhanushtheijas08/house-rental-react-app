422b3ca92f5e9ace1e8181abf2a635c449ef8ffa
dhanushtheijas08 <21cs033@kpriet.ac.in>
2024-05-23 14:09:49 +0530

    chore: Update .gitignore and firebase config

--- src/service/firebase-config.js
+++ src/service/firebase-config.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyABll5S8WtHJGzTf1nnxXBa_Yo7tt6MHJ8",
-  authDomain: "house-rental-69320.firebaseapp.com",
-  projectId: "house-rental-69320",
-  storageBucket: "house-rental-69320.appspot.com",
-  messagingSenderId: "708321506900",
-  appId: "1:708321506900:web:4fda3d0ebeaaa6efd09fc1",
-  measurementId: "G-TNGV4FKE5S",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
